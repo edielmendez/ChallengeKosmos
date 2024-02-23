d1632c3ea45e72d5108eae5371ca2b71ccc0d85d
Ediel Mendez <mendezjunior2015@gmail.com>
2024-02-23 13:14:38 -0600

    Init VCS

---
+++ app/src/main/java/mx/com/mvtech/kosmoschallenge/MainActivity.kt
@@ -0,0 +1,46 @@
+package mx.com.mvtech.kosmoschallenge
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import mx.com.mvtech.kosmoschallenge.ui.theme.KosmosChallengeTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            KosmosChallengeTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    KosmosChallengeTheme {
+        Greeting("Android")
+    }
+}
